9a9ae6bf6a9b53fff0519b5825bb8b47720f6608
akshookum <akshookum.nazieva.kk@gmail.com>
2023-07-19 05:04:48 +0300

    first commit

---
+++ bin/dart_bild_typeds.dart
@@ -0,0 +1,111 @@
+import 'package:dart_bild_typeds/dart_bild_typeds.dart' as dart_bild_typeds;
+
+void main(List<String> arguments) {
+//   print('Hello world: ${dart_bild_typeds.calculate()}!');
+  String str01 = 'Lesson';
+  print(str01);
+
+  var str02 = 'Scool';
+  print(str02);
+
+  var str03 = 'College';
+  print(str03);
+
+  var str04 = 'University';
+  print(str04);
+
+  var str05 = 'Gum';
+  print(str05);
+
+  var str06 = 'Mercury';
+  print(str06);
+
+  var str07 = 'Jupiter';
+  print(str07);
+
+  var str08 = 'Angel';
+  print(str08);
+
+  var str09 = 'Star';
+  print(str09);
+
+  var str10 = 'Love';
+  print(str10);
+
+  num num01 = 2;
+  print(num01);
+
+  num num02 = 4;
+  print(num02);
+
+  num num03 = 6;
+  print(num03);
+
+  num num04 = 20;
+  print(num01);
+
+  num num05 = 18;
+  print(num05);
+
+  int int01 = 15;
+  print(int01);
+
+  var int02 = 17;
+  print(int02);
+
+  int int03 = 19;
+  print(int03);
+
+  int int04 = 13;
+  print(int04);
+
+  int int05 = 11;
+  print(int05);
+
+  double double01 = 14.14;
+  print(double01);
+
+  double double02 = 16.12;
+  print(double02);
+
+  double double03 = 18.13;
+  print(double03);
+
+  double double04 = 22.15;
+  print(double04);
+
+  double double05 = 12.14;
+  print(double05);
+
+  bool bool01 = true; //false;(tuura,kata)
+  print(bool01);
+
+  bool bool02 = false;
+  print(bool02);
+
+  // print('$str01, $num01, $int01, $bool01, $double01');(baaryn bir salyy)
+
+  String firsName = 'Akshookum';
+  var lastName = 'Nazieva';
+  var tuulGanjer = 'Kara Kulja';
+  var kursTash = 'Flutter15';
+  print(
+      'Menin atym $firsName,familam $lastName, tuulgan jerim $tuulGanjer, uchurda okup jatkan kurs $kursTash');
+
+  //Num example
+  num san1 = 45;
+  num san2 = 35;
+  print('$san1 + $san2 = ${san1 + san2}');
+
+  num san3 = 45;
+  num san4 = 35;
+  print('$san3 - $san4 = ${san3 - san4}');
+
+  //Bool example
+  bool correct = true; //tuura
+  bool unCorrect = false; //kata
+  print('3 + 2 = 5 $correct');
+  print('5 - 2 = 4 $unCorrect');
+
+  //Dynamic
+}
---
+++ test/dart_bild_typeds_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_bild_typeds/dart_bild_typeds.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
